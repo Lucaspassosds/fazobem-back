04315641dd24346a24a0f8f2dbc0096259561cee
Lucas Passos <lsiqueira355@gmail.com>
2023-08-14 23:13:22 -0300

    feat(voluntary-role): voluntary-role entity and CRUD functionalities

---
+++ src/api/voluntary-role/entities/voluntary-role.entity.ts
@@ -0,0 +1,11 @@
+import { BaseTable } from 'src/api/common/entities/base.entity';
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class VoluntaryRole extends BaseTable {
+  @Column({ nullable: false, length: 100 })
+  name: string;
+
+  @Column({ nullable: true, length: 255 })
+  description: string;
+}
---
+++ src/api/voluntary-role/voluntary-role.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { VoluntaryRoleService } from './voluntary-role.service';
+import { CreateVoluntaryRoleDto } from './dto/create-voluntary-role.dto';
+import { UpdateVoluntaryRoleDto } from './dto/update-voluntary-role.dto';
+
+@Controller('voluntary-role')
+export class VoluntaryRoleController {
+  constructor(private readonly voluntaryRoleService: VoluntaryRoleService) {}
+
+  @Post()
+  create(@Body() createVoluntaryRoleDto: CreateVoluntaryRoleDto) {
+    return this.voluntaryRoleService.create(createVoluntaryRoleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.voluntaryRoleService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.voluntaryRoleService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateVoluntaryRoleDto: UpdateVoluntaryRoleDto,
+  ) {
+    return this.voluntaryRoleService.update(+id, updateVoluntaryRoleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.voluntaryRoleService.delete(+id);
+  }
+}
