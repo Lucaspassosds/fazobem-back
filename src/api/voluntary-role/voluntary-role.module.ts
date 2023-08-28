790406ded6db3956626b453573726ad50239503a
lucas <lucas@readyon.ai>
2023-08-28 11:35:16 -0300

    feat(voluntary-role): roles CRUD

--- src/api/voluntary-role/voluntary-role.module.ts
+++ src/api/voluntary-role/voluntary-role.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VoluntaryRoleService } from './voluntary-role.service';
 import { VoluntaryRoleController } from './voluntary-role.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VoluntaryRole } from './entities/voluntary-role.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VoluntaryRole])],
   controllers: [VoluntaryRoleController],
-  providers: [VoluntaryRoleService]
+  providers: [VoluntaryRoleService],
 })
 export class VoluntaryRoleModule {}
--- src/api/voluntary-role/voluntary-role.service.ts
+++ src/api/voluntary-role/voluntary-role.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from '../common/services/base.service';
 import { VoluntaryRole } from './entities/voluntary-role.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class VoluntaryRoleService extends BaseService<VoluntaryRole> {}
+export class VoluntaryRoleService extends BaseService<VoluntaryRole> {
+  constructor(
+    @InjectRepository(VoluntaryRole)
+    voluntaryRoleRepository: Repository<VoluntaryRole>,
+  ) {
+    super(voluntaryRoleRepository);
+  }
+}
